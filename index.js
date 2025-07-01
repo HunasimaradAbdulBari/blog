8a6c2d7f35457d9242b28c9afbee339d503301d9
HunasimaradAbdulBari <sanoof151@gmail.com>
2025-07-01 15:00:53 +0530

    mid

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const express = require("express");
-const path = require("path"); // ✅ Add this line
-// const mongoose = require(mongoose);
-const userRoute =require('./routes/user');
-const { default: mongoose, mongo } = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
+// Import routes
+const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
+// Import models
+const { default: mongoose } = require("mongoose");
+const Blog = require("./models/blog");
+
 const app = express();
 const PORT = 8000;
 
-mongoose.connect('mongodb://localhost:27017/sanoofblog').then(e => console.log("mongoDB connected"));
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/sanoofblog')
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB connection error:", err));
+
+// View engine setup
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.urlencoded({ extended: false}));
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get('/', (req, res) => {
-  res.render('home');
+// Routes
+app.get('/', async (req, res) => {
+  const allBlogs = await Blog.find({}).populate("createdBy");
+  res.render('home', {
+    user: req.user,
+    blogs: allBlogs,
+  });
 });
 
-app.use('/user',userRoute);
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
